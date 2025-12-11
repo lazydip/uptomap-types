cec5be82faf9c64cd0b8f569e5c0ba3b319b3fc8
Shlomo Ashlag <lazydip@hotmail.com>
2025-12-11 22:18:27 +0200

    add unexpected tags Scope

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,5 +6,5 @@ export type LocationTags = Tags & {
     icon: string;
 };
 export type ProfileTags = Tags;
-export declare const TAG_SCOPE: readonly ["profile", "location"];
+export declare const TAG_SCOPE: readonly ["profile", "location", "unexpected"];
 export type TagsExructionScope = (typeof TAG_SCOPE)[number];
--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export type Tags = { tags: TagsList };
 export type LocationTags = Tags & { icon: string };
 export type ProfileTags = Tags;
 
-export const TAG_SCOPE = ["profile", "location"] as const;
+export const TAG_SCOPE = ["profile", "location", "unexpected"] as const;
 export type TagsExructionScope = (typeof TAG_SCOPE)[number];
