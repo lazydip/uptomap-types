29438761b0f3dd8ccc8bc146bc76de6c4f6b6dcd
Shlomo Ashlag <lazydip@hotmail.com>
2025-12-11 21:03:54 +0200

    add types for tags

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,10 @@
-export interface ExampleSharedType {
-    id: string;
-}
+export type TagsList = string[];
+export type Tags = {
+    tags: TagsList;
+};
+export type LocationTags = Tags & {
+    icon: string;
+};
+export type ProfileTags = Tags;
+export declare const TAG_SCOPE: readonly ["profile", "location"];
+export type TagsExructionScope = (typeof TAG_SCOPE)[number];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export interface ExampleSharedType {
-  id: string;
-}
+export type TagsList = string[];
+
+export type Tags = { tags: TagsList };
+
+export type LocationTags = Tags & { icon: string };
+export type ProfileTags = Tags;
+
+export const TAG_SCOPE = ["profile", "location"] as const;
+export type TagsExructionScope = (typeof TAG_SCOPE)[number];
